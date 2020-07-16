fa58d848203a94b9ebb0143dfd8208cd5449d9d0
Nishant <np210291@gmail.com>
2020-07-16 11:32:03 +0530

    First Inception

---
+++ services/APILayer.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import axios from 'axios';
+
+const ACCESS_TOKEN = '3194105207295251';
+const URL = `https://superheroapi.com/api/${ACCESS_TOKEN}`;
+const SUPERHERO_URL = 'http://www.superheroes-idb.tk/characters/';
+
+// Create axios client, pre-configured with our URLs
+let APILayer = axios.create({
+  timeout: 10000,
+});
+
+// Define our API call methods
+export const getCharacters = async name => {
+  console.log('Get Superhero character list', URL);
+  var result = [];
+  try {
+    const response = await APILayer.get(URL + `/search/${name}`);
+    console.log('Get Superhero character list', response.data.results[0]);
+    result = response.data.results;
+  } catch (err) {
+    console.log('ERRRR', err);
+    result = err;
+  }
+  return result;
+};
+
+export default APILayer;
